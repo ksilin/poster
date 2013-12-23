7da41c1b8deef2492c306f6ac6f486da3e9a3cc4
ksilin <konstantin.silin@gmail.com>
2013-12-23 07:13:14 +0100

    made poster gem executable

--- lib/poster.rb
+++ lib/poster.rb
@@ -7,15 +7,21 @@ module Poster
 
   # TODO control overwriting the target
   # TODO use .erb template
+  # TODO complete code block syntax in posts using either a defualt language or CL param
 
   def self.convert(files = Finder.find)
 
-    #files =  [*filename] ||
     p "found #{files.size} files to convert"
     files.map{|f|
-      p "posting #{f}"
+      p "preparing #{f} for posting"
       # TODO - this check comes too late, all files must have Date-parseable names
-      date = Date.parse(f) rescue next
+      date = Time.now
+      begin
+      date = Date.parse(f)
+      rescue => e
+        p "unable to parse #{f} for a date, falling back to #{date}"
+      end
+
       posts = Splitter.split(File.open(f).read)
       posts.each{|post|
         Planter.create(Splitter.title(post), post, date)
