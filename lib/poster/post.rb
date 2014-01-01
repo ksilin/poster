6b8ac3e2431b05bce5b18ceae40c814c17965e21
ksilin <konstantin.silin@gmail.com>
2014-01-01 15:22:02 +0100

    added Post class and spec

---
+++ lib/poster/post.rb
@@ -0,0 +1,47 @@
+module Poster
+  class Post
+
+    # TODO add tags/categories
+
+    attr_reader :title, :author, :content, :created_at, :posted_at
+
+    # TODO - how to post from different authors?
+    def initialize(title = 'Title could not be found', author = ENV['USERNAME'], content = 'lorem ipsum', created_at = Time.now, posted_at = Time.now )
+      @title = title
+      @author = author
+      @content = content
+
+      @created_at = created_at
+      @posted_at = posted_at
+    end
+
+    def header
+      h = []
+      h << '---'
+      h << 'layout: post'
+      h << "title: \"#{title.gsub(/&/, '&amp;')}\""
+      h << "date: #{posted_at}"
+      h << "author: #{author}"
+      # TODO - make 'published' configurable over CL
+      h << 'published: true'
+      h << 'comments: true'
+      h << 'categories: '
+      h << '---'
+      h << ''
+      h.join("\n")
+    end
+
+    def filename
+      "#{created_at.year}-#{created_at.month}-#{created_at.day}-#{slug}.markdown"
+    end
+
+    private
+
+    # replace spaces with '-'
+    # remove non-word characters
+    def slug
+      title.split.join('-').gsub(/[^\w-]+/, '')
+    end
+
+  end
+end
---
+++ spec/post_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'spec_helper'
+#require 'active_support/core_ext/numeric/time'
+
+module Poster
+
+  describe Post do
+
+    it 'should create a valid post with default params' do
+      post = Post.new
+      expect(post.title).to eq 'Title could not be found'
+      expect(post.author).to eq ENV['USERNAME']
+      expect(post.content).not_to be_empty
+      expect(post.created_at).to be_between(Time.now - 60, Time.now)
+      expect(post.posted_at).to be_between(Time.now - 60, Time.now)
+    end
+
+    it 'should create a valid filename with default params' do
+      post = Post.new
+      filename = post.filename
+      expect(filename).to match /^(\d)+-+.*.markdown$/
+    end
+  end
+end
