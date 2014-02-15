5f071c928fcf9c7497eb258f6e0dd548946e50e9
ksilin <konstantin.silin@gmail.com>
2014-02-15 20:43:08 +0100

    improved parser spec

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,88 +5,100 @@ require 'date'
 module Poster
   describe Parser do
 
-    describe 'strange case of matching operators' do
-      it 'should match title delimiters' do
-        p 'abc' =~ /b/ # => 1
-        p 'abc' !=~/b/ # => true
-        p !('abc' =~ /b/) # => false
+    let(:no_posts){"bla \n text\n"}
+    let(:one_post){"### bla \n text\n"}
+    let(:two_posts){"### bla \n text\n ### blub \n more text\n"}
+
+    let(:one_post_after_plain_text){"hello ### goodbye"}
+
+    let(:wrong_delimiter){"***bla \n text\n"}
+    let(:too_short_delimiter){"## bla \n text\n"}
+    let(:too_long_delimiter){"#### bla \n text\n"}
+
+    describe 'splitting notes' do
+
+      it 'should split notes into posts' do
+
+        split = Parser.split(one_post)
+        expect(split).to have(1).item
       end
-    end
 
-    describe 'Split notes' do
+      it 'should split multiple posts' do
 
-      # TODO - excercise split with simple strings
-      it 'should split note into several posts' do
-
-        file = File.open(Dir.pwd + '/spec/assets/example_2013.12.21.md', 'r')
-        split = Parser.split(file.read)
-        split.each_with_index { |s, i|
-          p "#{i}: #{s}"
-        }
+        split = Parser.split(two_posts)
         expect(split).to have(2).items
       end
 
-      it 'should return an empty Array when the source string is empty' do
+      it 'should not find posts in an empty string' do
         split = Parser.split('')
         expect(split).to match_array []
       end
 
-      it 'should return an empty Array when file empty' do
+      it 'should not find posts when the source string does not contain delimiter' do
+        split = Parser.split(no_posts)
+        expect(split).to match_array []
+      end
 
+      it 'should not find posts when the string contains a different delimiter' do
+        split = Parser.split(wrong_delimiter)
+        expect(split).to match_array []
+      end
+
+      it 'should not find posts when the string contains a too short delimiter' do
+        split = Parser.split(too_short_delimiter)
+        expect(split).to match_array []
+      end
+
+      it 'should not find posts when the string contains a too long delimiter' do
+        split = Parser.split(too_long_delimiter)
+        expect(split).to match_array []
+      end
+
+      # this is kinda obvious. Am I testing File.read here?
+      it 'should not find posts in an empty file' do
         with_tempdir_and_files([:foo]) { |dir, files|
           split = Parser.split(File.open(files[0]).read)
           expect(split).to match_array []
         }
       end
 
-      it 'should return an empty Array when title delimiter not found' do
-
-        with_tempdir_and_files([:foo]) { |dir, files|
-          split = Parser.split(File.open(files[0]).read)
-          expect(split).to match_array []
-        }
-      end
     end
 
     describe 'title extraction' do
 
       it 'should extract titles' do
 
-        file = File.open(File.dirname(__FILE__) + '/assets/example_2013.12.21.md', 'r')
-        split = Parser.split(file.read)
+        split = Parser.split(one_post)
         titles = split.map { |post| Parser.title(post) }
-        expect(titles).to match_array ['The first post', 'The second post']
+        expect(titles).to match_array ['bla']
       end
 
-      it 'should extract titles' do
-
-        split = Parser.split('### hello')
+      it 'should extract multiple titles' do
+        split = Parser.split(two_posts)
         titles = split.map { |post| Parser.title(post) }
-        expect(titles).to match_array ['hello']
+        expect(titles).to match_array ['bla', 'blub']
       end
 
       it 'should drop everything before the first title' do
 
-        split = Parser.split(" hello \n and ### goodbye")
+        split = Parser.split(one_post_after_plain_text)
         titles = split.map { |post| Parser.title(post) }
         expect(titles).to match_array ['goodbye']
       end
 
-      # not sure what to do with fiels that do not have s single title in them
+      # not sure what to do with files that do not have s single title in them
       # probably the best thing would be to try different title delimiters and
       # suggest the most probably candidates
-      #  what char is found 3 or 4 times most in a row that is followed by a blank line
+      # what char is found 3 or 4 times most in a row that is followed by a blank line
 
       it 'only three hashes are a title' do
-
         split = Parser.split(" # a \n ## b \n #### c")
         expect(split).to match_array []
       end
 
 
       it 'should drop everything before the first title' do
-
-        split = Parser.drop_before_first_title(" hello \n and ### goodbye")
+        split = Parser.drop_before_first_title(one_post_after_plain_text)
         expect(split).to match '### goodbye'
       end
 
@@ -96,19 +108,6 @@ module Poster
 
       it 'should support configurable title format'
 
-    end
-
-    describe 'date parsing' do
-
-      it 'should extract dates' do
-        date = Date.parse('/assets/example_2013.12.21.md')
-        p date
-        p "y: #{date.year}, m: #{date.month}, d: #{date.day}"
-      end
-
-      it 'should fail on ambiguous dates' do
-        expect { Date.parse('/assets/example_4_2013_1_21_2.md') }.to raise_error(ArgumentError)
-      end
     end
 
     # the tags are written on the last non-space line of the post. Format: [tag1, tag2], whitespace optional
