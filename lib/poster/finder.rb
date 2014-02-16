cbd19d32adf6ce8a37f111741ffacce15c27f4b8
ksilin <konstantin.silin@gmail.com>
2014-02-16 14:26:01 +0100

    removed validation form finder and test

--- lib/poster/finder.rb
+++ lib/poster/finder.rb
@@ -1,36 +1,27 @@
 require 'find'
+require_relative 'validator'
 
 module Poster
   class Finder
 
-    # ends in .md or .markdown
-    BASENAME = /^([0-9a-zA-Z_\-\/\.])+([0-9a-zA-Z_])\.m(d|arkdown)$/
-    EXTENSIONS = /^\.m(d|arkdown)$/
-    DATES = /\d{4}_\d{2}_\d{2}/
+    def self.find(dir = Dir.pwd, opts = {})
+      opts = { recursive: false, verbose: false }.merge(opts)
 
-    def self.find(dir = Dir.pwd, recursive = false)
       Find.find(dir).select { |f|
-        if do_not_recurse(dir, f, recursive)
-          p "pruning #{f.inspect}"
-          Find.prune
-        end
-        File.file?(f) && has_valid_extension(f) && date_parseable(f)
+        prune(dir, f, opts)
+        valid_post_file?(f)
       }
     end
 
-    def self.has_valid_extension(f)
-      puts "matching #{File.extname(f)} with #{EXTENSIONS}"
-      EXTENSIONS =~ File.extname(f)
+    def self.valid_post_file?(f)
+      File.file?(f) && Validator.valid_post_filename?(f)
     end
 
-    def self.date_parseable(f)
-      basename = File.basename(f, File.extname(f))
-      # Date#parse seems no to accept underscores as date delimiters
-      replaced_undescores = basename.gsub('_', '-')
-      # This is far too lenient. I want dates to be digits only
-      # But then I would need to vary delimiters
-      # TODO - ask on SO how to make such date parsing with different delimiters
-      Date.parse(replaced_undescores) rescue false
+    def self.prune(dir, f, opts)
+      if do_not_recurse(dir, f, opts[:recursive])
+        $stderr.puts "pruning #{f.inspect}" if opts[:verbose]
+        Find.prune
+      end
     end
 
     def self.do_not_recurse(dir, file, recursive)
--- spec/finder_spec.rb
+++ spec/finder_spec.rb
@@ -5,37 +5,38 @@ require 'find'
 module Poster
   describe Finder do
 
-    let(:files) { %w(foo_2014_02_15.md bar_2014_02_15.markdown) }
-    let(:not_as_extension) { %w(foo.md.exe bar.markdown.pdf) }
-    let(:dotfiles) { %w(.md .markdown) }
+    let(:markdown_files) { %w(foo_2014_02_15.md bar_2014_02_15.markdown) }
+    let(:other_extensions) { %w(foo_2014_02_15.mad bar_2014_02_15.murkdown) }
+    let(:not_as_extension) { %w(foo_2014_02_15.md.exe bar_2014_02_15.markdown.pdf) }
+    let(:dotfiles) { %w(.2014_02_15.md .2014_02_15.markdown) }
 
     let(:nested) { %w(foo_dir/foo_2014_02_15.md bar_dir/even_deeper/bar_2014_02_15.markdown) }
-
-    let(:with_garbage) { list = to_filelist('foo.md')
-    list['foo.md'] = '123423644tzerztbertzber'
-    list
-    }
 
     describe 'identifying markdown files' do
 
       it 'should find all .md and .markdown files' do
-        with_tempdir(files) { |dir|
-          found = Finder.find(dir)
-          expect(found[0]).to match /(\w)+.markdown$/
-          expect(found[1]).to match /(\w)+.md$/
-        }
+        with_tempdir(markdown_files) do |dir|
+          expect(Finder.find(dir)).to have(2).items
+        end
+      end
+
+      it 'should find all .md and .markdown files' do
+        with_tempdir(other_extensions) do |dir|
+          expect(Finder.find(dir)).to be_empty
+        end
       end
 
       it 'should not find files with containing .md and .markdown but not as extensions' do
-        with_tempdir(not_as_extension) { |dir|
+        with_tempdir(not_as_extension) do |dir|
           expect(Finder.find(dir)).to be_empty
-        }
+        end
       end
 
-      it 'should not find dotfiles called .md or .markdown' do
-        with_tempdir(dotfiles) { |dir|
-          expect(Finder.find(dir)).to be_empty
-        }
+      it 'should not find dotfiles' do
+        with_tempdir(dotfiles) do |dir|
+          found = Finder.find(dir)
+          expect(found).to be_empty
+        end
       end
     end
 
@@ -44,48 +45,35 @@ module Poster
     describe 'identifying files with parseable date in the filename' do
 
       it 'should ignore files without a parseable dates in the name' do
-        with_tempdir(['nodate.md']) { |dir|
+        with_tempdir(['nodate.md']) do |dir|
           expect(Finder.find(dir)).to eq []
-        }
+        end
       end
 
       it 'should find files with a parseable dates in the name' do
-        with_tempdir(['notes_2014.01.29.md']) { |dir|
+        with_tempdir(['notes_2014.01.29.md']) do |dir|
           expect(Finder.find(dir)).to have(1).item
-        }
+        end
       end
-
-      it 'should not find dates where there are none' do
-          expect(Finder.date_parseable('nodate.md')).to be false
-      end
-
-      it 'should fail where Date.parse fails'
-
-      it 'should accept files with parseable dates in the name' do
-        expect(Finder.date_parseable('notes_2014.01.29.md')).to be_true
-      end
-
-
     end
 
 
     describe 'recursive search' do
 
       it 'should find files in nested dirs ' do
-        with_tempdir(nested) { |dir|
-          found = Finder.find(dir, true)
-          puts "found: #{found.join('\n')}"
+        with_tempdir(nested) do |dir|
+          found = Finder.find(dir, recursive: true)
           expect(found).to have(2).items
-        }
+        end
       end
     end
 
     describe 'non-recursive search' do
 
       it 'should ignore files in nested dirs ' do
-        with_tempdir(nested) { |dir|
+        with_tempdir(nested) do |dir|
           expect(Finder.find(dir)).to be_empty
-        }
+        end
       end
     end
 
