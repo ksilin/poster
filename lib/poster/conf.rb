8863b7618be23933ff93745c1bf166c3f851b48b
ksilin <konstantin.silin@gmail.com>
2014-07-15 07:04:43 +0200

    a little fiddling with CLI specs

--- lib/poster/conf.rb
+++ lib/poster/conf.rb
@@ -9,13 +9,13 @@ class Conf
 
   def initialize(config_file = CONFIG_FILE)
     user_config = File.join(Dir.home, CONFIG_FILENAME)
-    puts "found config in home dir:  #{File.exist? user_config} : #{user_config}"
+    $stderr.puts "found config in home dir:  #{File.exist? user_config} : #{user_config}"
     folder_config = File.join(Dir.pwd, CONFIG_FILENAME)
-    puts "found config in working dir:  #{File.exist? folder_config} : #{folder_config}"
+    $stderr.puts "found config in working dir:  #{File.exist? folder_config} : #{folder_config}"
 
     if File.exist? config_file
       @config_options = YAML.load_file(config_file)
-      $stderr.puts "loaded #{config_file}"
+      $stderr.puts "loaded #{config_file}, #{@config_options}"
       # options.merge!(config_options)
     else
       warn "configuration not found in #{config_file}"
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,13 +1,24 @@
 require 'rspec'
 require 'open3'
+require 'yaml'
 
 describe 'simple' do
+
+  let(:assets) { File.join(File.dirname(__FILE__), 'assets') }
+  let(:command) { "bin/poster -h #{assets}" }
+  let(:list_settings) { "bin/poster -l #{assets}" }
+
+  it 'context' do
+    # for some reason, the working dir is not the spec dir:
+    puts "working dir: #{Dir.pwd}"
+    # => ~/Code/Ruby/workspaces/poster
+    puts "the assets are at: #{assets}"
+  end
 
   it 'should start and run' do
 
     # TODO: expect no files
 
-    command = 'bin/poster -h ~/Desktop/md/done'
     Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
       pid = wait_thr.pid # pid of the started process.
       exit_status = wait_thr.value # Process::Status object returned.
@@ -25,11 +36,47 @@ describe 'simple' do
       # puts stdin.read
     end
 
-    # p system('bin/poster')
-
-    # TODO: capture and eval help output
-
     # TODO: expect files
 
   end
+
+  it 'should render help output to stdout' do
+    Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
+      output = stdout.read
+
+      expect(output).to match /-n, --dry-run/
+      expect(output).to match /-v, --verbose/
+    end
+  end
+
+  it 'should render list of settings to stdout' do
+    Open3.popen3(list_settings) do |_stdin, stdout, stderr, wait_thr|
+      output = stdout.read
+      expect(output).to match /:test=>nil/
+    end
+  end
+
+  it 'should find and use settings file in working dir' do
+    mock_config = File.join(assets, '.poster.rc.yaml')
+    File.open(mock_config, 'w') do |f|
+      content = { :source_dir => "source", :posts_dir => "_posts", :blogs => { :test => nil }, :default_blog => :test }
+      yaml = content.to_yaml
+      puts "writing to config: #{yaml}"
+      f.write yaml
+    end
+
+    Dir.chdir(assets) do
+
+      Open3.popen3(File.join('..', '..', list_settings)) do |_stdin, stdout, stderr, wait_thr|
+        output = stdout.read
+        err = stderr.read
+        expect(output).to match /:test=>nil/
+        expect(err).to match /found config in working dir:  true/
+        expect(err).to match /wef/
+      end
+    end
+    ensure
+    File.delete(mock_config)
+  end
+
 end
