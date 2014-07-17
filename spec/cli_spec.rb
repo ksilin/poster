cc5faca67e82cea38dfce0ece45ca180bd3f273e
ksilin <konstantin.silin@gmail.com>
2014-07-17 06:09:02 +0200

    made paths for specs more robust

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -5,8 +5,9 @@ require 'yaml'
 describe 'simple' do
 
   let(:assets) { File.join(File.dirname(__FILE__), 'assets') }
-  let(:command) { "bin/poster -h #{assets}" }
-  let(:list_settings) { "bin/poster -l #{assets}" }
+  let(:executable) { File.join(File.dirname(__FILE__), '..', 'bin', 'poster') }
+  let(:help) { executable + " -h #{assets}" }
+  let(:list_settings) { executable + " -l #{assets}" }
 
   it 'context' do
     # for some reason, the working dir is not the spec dir:
@@ -19,7 +20,7 @@ describe 'simple' do
 
     # TODO: expect no files
 
-    Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
+    Open3.popen3(help) do |_stdin, stdout, stderr, wait_thr|
       pid = wait_thr.pid # pid of the started process.
       exit_status = wait_thr.value # Process::Status object returned.
 
@@ -41,7 +42,7 @@ describe 'simple' do
   end
 
   it 'should render help output to stdout' do
-    Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
+    Open3.popen3(help) do |_stdin, stdout, stderr, wait_thr|
       output = stdout.read
 
       expect(output).to match /-n, --dry-run/
@@ -57,26 +58,30 @@ describe 'simple' do
   end
 
   it 'should find and use settings file in working dir' do
-    mock_config = File.join(assets, '.poster.rc.yaml')
-    File.open(mock_config, 'w') do |f|
-      content = { :source_dir => "source", :posts_dir => "_posts", :blogs => { :test => nil }, :default_blog => :test }
-      yaml = content.to_yaml
-      puts "writing to config: #{yaml}"
-      f.write yaml
-    end
+    begin
 
-    Dir.chdir(assets) do
-
-      Open3.popen3(File.join('..', '..', list_settings)) do |_stdin, stdout, stderr, wait_thr|
-        output = stdout.read
-        err = stderr.read
-        expect(output).to match /:test=>nil/
-        expect(err).to match /found config in working dir:  true/
-        expect(err).to match /wef/
+      mock_config = File.join(assets, '.poster.rc.yaml')
+      File.open(mock_config, 'w') do |f|
+        content = { :source_dir => 'source', :posts_dir => '_posts', :blogs => { :test => nil }, :default_blog => :test }
+        yaml = content.to_yaml
+        puts "writing to config: #{yaml}"
+        f.write yaml
       end
-    end
+
+      Dir.chdir(assets) do
+
+        Open3.popen3(list_settings) do |_stdin, stdout, stderr, wait_thr|
+          output = stdout.read
+          err = stderr.read
+          expect(output).to match /:test=>nil/
+          expect(err).to match /found config in working dir:  true/
+          expect(err).to match /wef/
+        end
+      end
+
     ensure
-    File.delete(mock_config)
+      File.delete(mock_config)
+    end
   end
 
 end
